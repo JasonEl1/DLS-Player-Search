fe3e900460777bb2ba563d547c95bda52e7cbdea
MTN73 <87441196+MTN73@users.noreply.github.com>
2023-04-17 23:07:37 -0400

    Broader search and preparing player img search

--- playerSearch.js
+++ playerSearch.js
@@ -1,24 +1,33 @@
-const url = 'https://raw.githubusercontent.com/theshinramen/DLS-Files/main/data/playerData.json';
+const players = 'https://raw.githubusercontent.com/theshinramen/DLS-Files/main/data/playerData.json';
+const imgs = 'https://raw.githubusercontent.com/theshinramen/DLS-Files/main/playerphotos/DLS23/';
+
+const card = 'https://raw.githubusercontent.com/theshinramen/DLS-Card-Creator/main/DLS23/assets/card/';
+//common, common GK, rare, rare GK, legendary, legendary GK
+const cards = ['common.png','commonGK.png','rare.png','rareGK.png','legendary.png','legendaryGK.png']
 
 document.getElementById("searchbtn").addEventListener("click", searchPlayer);
 
 function searchPlayer(){
+    
+document.getElementById("result").innerHTML = "";
 
-fetch(url)
+fetch(players)
 .then(response => response.json())
 .then(data => {
     const playerData = data;
     const player_fname = document.getElementById("fnamesearch").value;
     const player_lname = document.getElementById("lnamesearch").value;
-    let found = false;
+    let found = 0;
 
     for(const player of data){
-        if(player["First Name"].toLowerCase()==player_fname.toLowerCase() && player["Last Name"].toLowerCase()==player_lname.toLowerCase()){
-            document.getElementById("result").innerHTML = JSON.stringify(player);
-            found = true;
+        if(player["First Name"].toLowerCase().includes(player_fname.toLowerCase()) && player["Last Name"].toLowerCase().includes(player_lname.toLowerCase())){
+            document.getElementById("result").innerHTML += JSON.stringify(player);
+            document.getElementById("result").innerHTML += "<br></br>"
+            found++;
         }
     }
-    if(!found){
+    document.getElementById("count").innerHTML = found + " Results";
+    if(found==0){
         document.getElementById("result").innerHTML = "Player not found."
     }
 });
